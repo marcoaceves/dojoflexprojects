fa211f59b926a30bb27d163ccce139df8ca48463
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2021-10-01 16:15:12 -0700

    friday

--- Dojo Flex class projects/week_5/likes/script.js
+++ Dojo Flex class projects/week_5/likes/script.js
@@ -1,6 +1,18 @@
-var count = 3;
+var countOne = 3;
 var countElement = document.querySelector(".likesOne")
 function add1() {
-    count++;
-    countElement.innerText = count + " like(s)";
+    countOne++;
+    countElement.innerText = countOne + " like(s)";
+}
+var countTwo = 12;
+var countElement2 = document.querySelector( ".likesTwo")
+function add2() {
+    countTwo++;
+    countElement2.innerText = countTwo + " like(s)";
+}
+var countThree = 9;
+var countElement3 = document.querySelector( ".likesThree")
+function add3() {
+    countThree++;
+    countElement3.innerText = countThree + " like(s)";
 }
