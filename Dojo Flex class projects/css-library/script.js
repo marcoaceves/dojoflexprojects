2a1e3561d75da516b093d25555f4feca85b9a9bd
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2021-10-01 14:16:21 -0700

    coding day

--- Dojo Flex class projects/css-library/script.js
+++ Dojo Flex class projects/css-library/script.js
@@ -1,6 +1,27 @@
+// Sticky notes 
+
 function addShadows(element) {
     element.classList.add("shadow");
 }
 function removeShadow(element) {
     element.classList.remove("shadow");
+}
+
+
+
+// counter
+
+var count = 1;
+var countElement = document.querySelector("#count")
+function add1() {
+    count++;
+    countElement.innerText = "The count is " + count;
+
+}
+var count = 1;
+var countElement = document.querySelector("#count")
+function subtract1() {
+    count--;
+    countElement.innerText = "The count is " + count;
+
 }
---
+++ Dojo Flex class projects/week_5/likes/script.js
@@ -0,0 +1,6 @@
+var count = 3;
+var countElement = document.querySelector(".likesOne")
+function add1() {
+    count++;
+    countElement.innerText = count + " like(s)";
+}
