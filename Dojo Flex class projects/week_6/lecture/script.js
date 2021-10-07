9cb7200e54dd1e6d7b3134b4b42ac7c32def3149
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2021-10-06 18:10:53 -0700

    lecture update

---
+++ Dojo Flex class projects/week_6/lecture/script.js
@@ -0,0 +1,20 @@
+
+// var number = 7
+
+// console.log(6 + number)
+
+// var strOne = "Hello ";
+// var strTwo = "World ";
+// var strtree = "Marco";
+// console.log(strOne + strTwo   + strtree);        
+
+// var str = '"It`s been a grean night of coding"'
+// console.log(str)
+
+// var bool = true 
+
+var arr = ["marco ", "aceves", 22, 12, 12 ]
+console.log(arr[0] + arr[1])
+console.log(arr.length)
+
+console.log(arr.length + 9);
