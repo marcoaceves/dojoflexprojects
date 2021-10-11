071a24ae33ead5c181c8e7c5b1f7ad0050667c8e
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2021-10-10 21:26:31 -0700

    terminus

--- Dojo Flex class projects/week_5/likes/script.js
+++ Dojo Flex class projects/week_5/likes/script.js
@@ -1,18 +1,24 @@
-var countOne = 3;
-var countElement = document.querySelector(".likesOne")
-function add1() {
-    countOne++;
-    countElement.innerText = countOne + " like(s)";
+var countOne = [3,12,9];
+var countElement = [
+    document.querySelector(".likesOne"),
+    document.querySelector(".likesTwo"),
+    document.querySelector(".likesThree")
+]
+
+function add1(i) {
+
+    countOne[i]++;
+    countElement[i].innerText = countOne[i] + " like(s)";
 }
-var countTwo = 12;
-var countElement2 = document.querySelector( ".likesTwo")
-function add2() {
-    countTwo++;
-    countElement2.innerText = countTwo + " like(s)";
-}
-var countThree = 9;
-var countElement3 = document.querySelector( ".likesThree")
-function add3() {
-    countThree++;
-    countElement3.innerText = countThree + " like(s)";
-}
+// var countTwo = 12;
+// var countElement2 = document.querySelector( ".likesTwo")
+// function add2() {
+//     countTwo++;
+//     countElement2.innerText = countTwo + " like(s)";
+// }
+// var countThree = 9;
+// var countElement3 = document.querySelector( ".likesThree")
+// function add3() {
+//     countThree++;
+//     countElement3.innerText = countThree + " like(s)";
+// }
