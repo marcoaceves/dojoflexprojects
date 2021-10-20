6725894347d0dc0769e14514cd668d2e946607e1
MarcoAceves <mr.aceves@gmail.com>
2021-10-20 16:39:19 -0700

    ARRAY CHALLENGES

---
+++ week_7/Array Challenges/script.js
@@ -0,0 +1,17 @@
+function alwaysHungry(arr) { 
+    for( var i = 0; i < 5; i++){
+        if(arr[i] === "food"){
+        console.log("yummy");}
+        
+        }
+
+    }
+            
+            
+
+
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
---
+++ week_7/infinite loop/scrip.js
@@ -0,0 +1,9 @@
+let num = 0
+
+while (num <= 20){ 
+    console.log(num)
+    num++
+    
+}
+
+
